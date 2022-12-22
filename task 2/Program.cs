f5bea4b7a3543dff4941d3ba82ba4ca5b8b12ebc
dimon777ddd <gxgamer727@gmail.com>
2022-12-22 20:36:36 +0400

    task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿//second task
+int i = 0;
+int max_number = 0;
+while (i < 3)
+{
+    Console.Write($"Enter {i+1} number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > max_number)
+    {
+        max_number = number;
+    }
+       i++; 
+}
+Console.WriteLine($"Max number = {max_number}");

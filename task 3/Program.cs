085a2060ef0882be363f9e45a903103e0406d163
dimon777ddd <gxgamer727@gmail.com>
2022-12-22 20:35:21 +0400

    task 3

---
+++ task 3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write($"Enter how many numbers you want to check:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+while (i < numbers)
+{
+    Console.Write($"Enter number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"Number {number} is even");
+    }
+    else
+    {
+        Console.WriteLine($"Number {number} is odd");
+    }
+        i++;
+}

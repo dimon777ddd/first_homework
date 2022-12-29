53fdd2c04f73be6ac7713c44455568b104309efe
dimon777ddd <gxgamer727@gmail.com>
2022-12-29 21:54:17 +0400

    3rd seminar

---
+++ 3rd_seminar/task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter number: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+
+  {
+    Console.WriteLine($"Your number: {number} is palindrom.");
+  }
+
+  else Console.WriteLine($"Your number: {number} is not palindrom.");
+}
+
+if (number.Length == 5)
+{
+  CheckingNumber(number);
+}
+
+else Console.WriteLine($"Enter corect number");
